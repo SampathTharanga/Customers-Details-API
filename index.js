ab3a88d41d8f08fdca0dedca61c935db80b4dcaa
SampathTharanga <sampaththaranga13@gmail.com>
2021-05-27 15:26:20 +0530

    set the application listening

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = express()
 const port =3000
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+
+app.listen(port, ()=> {
+  console.log(1`Custoemrs Details Appliction listening at http://localhost:${port}`)
+})
