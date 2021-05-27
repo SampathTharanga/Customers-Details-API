4b44814c0bd31d8aa79c354ed7046052968e2441
SampathTharanga <sampaththaranga13@gmail.com>
2021-05-27 15:57:45 +0530

    GET request working

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+app.get('/api/customer', (req, res) => {
+  res.send([1, 2, 3]);
+});
 
 app.listen(3000), () => console.log('Listening on port 3000...');
