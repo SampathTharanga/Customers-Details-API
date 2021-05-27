27dc3103b3f6ffee5ca1edb274b334cd520284d5
SampathTharanga <sampaththaranga13@gmail.com>
2021-05-27 15:53:02 +0530

    setup server

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 console.log(`Hello Node.js v${process.versions.node}!`);
 
-app.listen(port, () => {
-  console.log(
-    `Custoemrs Details Appliction listening at http://localhost:${port}`
-  );
-});
+app.listen(3000), () => console.log('Listening on port 3000...');
