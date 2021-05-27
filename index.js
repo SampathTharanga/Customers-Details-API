1cbd12ca19033449200a4eef923f4aeb805c412a
SampathTharanga <sampaththaranga13@gmail.com>
2021-05-27 23:24:02 +0530

    api/customer endpoint complete

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,4 +9,11 @@ app.get('/api/customer', (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000), () => console.log('Listening on port 3000...');
+//  /api/customer/1
+app.get('/api/customer/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+//PORT
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
