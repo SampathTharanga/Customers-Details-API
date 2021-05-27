cb15445942005ad426ae7b0cd3bcb0eb0c427548
SampathTharanga <sampaththaranga13@gmail.com>
2021-05-27 15:27:30 +0530

    Application running on port 3000

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
-const port =3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
-
-app.listen(port, ()=> {
-  console.log(1`Custoemrs Details Appliction listening at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(
+    `Custoemrs Details Appliction listening at http://localhost:${port}`
+  );
+});
