07254fddf17328261be28ede4986de72129f26a6
Sampath Tharanga <sampaththaranga13@gmail.com>
2021-05-30 15:56:26 +0530

    Data Update function write.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi')
 const express = require('express');
+const { custom } = require('joi');
 const app = express();
 
 app.use(express.json());
@@ -35,6 +36,7 @@ app.get('/api/posts/:year/:month', (req, res) => {
 
 app.post('/api/customers', (req, res) => {
   
+  /*
   const schema = Joi.object({
     name: Joi.string().min(3).required()
   })
@@ -45,8 +47,17 @@ app.post('/api/customers', (req, res) => {
 if(result.error) {
   //400 Bad Request
   res.status(400).send(result.error.details[0].message)
-}
+  return
+}*/
  
+const {error}  = validateCustomer(req.body)// result.error
+if(error) {
+  res.status(400).send(error.details[0].message)
+  return
+}
+
+
+
  /*
   if(!req.body.name || req.body.name.length < 3) {
     //400 Bad Request
@@ -62,6 +73,36 @@ if(result.error) {
   customers.push(customer);
   res.send(customer);
 });
+
+app.put('/api/customers/:id', (req, res) => {
+  // Look up the customer
+  //If not existing, return 404
+  var customer = customers.find(c => c.id === parseInt(req.params.id))
+  if (!customer)
+    res.status(404).send('The Custoemr with the givrn ID was not found.')
+
+
+  //Validate
+  //If invalide, return 400 - Bad request
+ const {error}  = validateCustomer(req.body)// result.error
+  if(error) {
+    res.status(400).send(error.details[0].message)
+    return
+  }
+  //Update customer
+  customer.name = req.body.name;
+  //Return the update customer
+  res.send(customer)
+})
+
+function  validateCustomer(customer) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  })
+
+  return schema.validate(customer)
+}
+
 
 //PORT
 const port = process.env.PORT || 3000
